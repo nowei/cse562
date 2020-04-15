6d042acf6682d88372647fe74d429f81ac7b9189
Andrew Wei <andrewwei1999@gmail.com>
2020-04-15 02:16:17 -0700

    need to change update to sliding window, then take FFT

--- hw1/app/src/main/java/com/example/cse562hw1/MainActivity.java
+++ hw1/app/src/main/java/com/example/cse562hw1/MainActivity.java
@@ -6,7 +6,9 @@ import android.graphics.Color;
 import android.media.AudioAttributes;
 import android.media.AudioFormat;
 import android.media.AudioManager;
+import android.media.AudioRecord;
 import android.media.AudioTrack;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -26,20 +28,50 @@ public class MainActivity extends AppCompatActivity {
     private AudioTrack wave = null;
     ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
     ScheduledFuture<?> detectionHandle = null;
-
+    private int fsmState = 0;
+    final Runnable detector = new Runnable() { public void run() { detection(); } };
+    final int BUFFER_SIZE = 2048;
+    final AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                    44100,
+                    AudioFormat.CHANNEL_IN_STEREO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    BUFFER_SIZE);
     public void updateDetection() {
-        final Runnable detector = new Runnable() {
-            public void run() {
-                TextView s = (TextView) findViewById(R.id.detection);
-                if (s.getText().toString().equals("None")) {
-                    s.setText(R.string.detection_push);
-                } else {
-                    s.setText(R.string.detection_none);
-                }
-            }
-        };
-        detectionHandle = scheduler.scheduleAtFixedRate(detector, -1, 10, MILLISECONDS);
+        detectionHandle = scheduler.schedule(detector, -1, MILLISECONDS);
+    }
 
+    public void detection() {
+//        int bufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT);
+        long start = System.currentTimeMillis();
+        try {
+
+            final byte[] buffer = new byte[BUFFER_SIZE];
+
+            recorder.startRecording();
+
+            // Read until buffer is full
+            int offset = 0;
+            int length = BUFFER_SIZE;
+            int read;
+            while (length > 0) {
+                read = recorder.read(buffer, offset, length);
+                length -= read;
+                offset += read;
+            }
+            recorder.stop();
+        } catch (Exception e) {
+            Log.d("In thread", e.toString());
+        }
+
+        // Perform FFT
+//                ditfft2(x, N, s);
+
+        TextView s = (TextView) findViewById(R.id.detection);
+//        s.setText(("Recordings done: " + fsmState++));
+        s.setText(("Time taken " + (System.currentTimeMillis() - start)));
+        if (enabled) {
+            detectionHandle = scheduler.schedule(detector, -1, MILLISECONDS);
+        }
     }
 
     @Override
@@ -47,6 +79,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         wave = generateTone();
+        wave.setPlaybackPositionUpdateListener(new AudioTrack.OnPlaybackPositionUpdateListener() {
+            @Override
+            public void onMarkerReached(AudioTrack track) {
+                if (enabled) {
+                    wave.pause();
+                    wave.flush();
+                }
+            }
+
+            @Override
+            public void onPeriodicNotification(AudioTrack track) {
+
+            }
+        });
     }
 
     /** Called to turn on and off the sound */
@@ -55,21 +101,11 @@ public class MainActivity extends AppCompatActivity {
         try {
             if (enabled) {
                 enabled = false;
-                wave.pause();
-                wave.flush();
                 Log.d("Toggling", "Turning off");
                 statusText.setText(R.string.status_off);
                 statusText.setTextColor(Color.RED);
                 AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-                scheduler.schedule(new Runnable() {
-                    public void run() {
-                        try {
-                            detectionHandle.cancel(true);
-                        } catch (Exception e) {
-                            Log.d("scheulderRunnable", e.toString());
-                        }
-                    }
-                }, 1, SECONDS);
+//                scheduler.schedule(new Runnable() { public void run() { detectionHandle.cancel(true); } }, 1, SECONDS);
             } else {
                 enabled = true;
                 wave.play();
@@ -82,6 +118,8 @@ public class MainActivity extends AppCompatActivity {
             Log.d("Toggling error", e.toString());
         }
     }
+
+
 
     private AudioTrack generateTone() {
         final int FREQUENCY = 18000;                     // in Hz
